414c4104538a067d5599e2b9f02cc792e452e241
Kyongsub Kong <s3634359@student.rmit.edu.au>
2020-03-04 18:27:30 +1100

    photo-studio

--- js/instafeed-custom.js
+++ js/instafeed-custom.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var l = 6;
+    var l = 10;
     
     var userFeed = new Instafeed({
         get: 'user',
@@ -8,15 +8,13 @@ $(document).ready(function() {
         resolution: 'standard_resolution',
         accessToken: '2267354727.1677ed0.4a5f3391d8fa4cd596995ebb404beea0',
         sortBy: 'most-recent',
-        template: '<div style="background-image: url({{image}});" class="insta_photo"></div>',
+        //template: '<div style="background-image: url({{image}});"></div>',
+        template: '<div class="grid-item portraits"><div class="thumbnail img-scale-in" data-hover-easing="easeInOut" data-hover-speed="500" data-hover-bkg-color="#000000" data-hover-bkg-opacity="0.4"><a class="overlay-link lightbox-link" data-group="gallery-1"data-image-url="http://sartre.thememountain.com/images/photo-studio/portfolio/grid/small-margins/project-8-lightbox.jpg" href="images/photo-studio/portfolio/grid/small-margins/project-9-lightbox.jpg"><img src="{{image}}" alt="" /><span class="overlay-info v-align-bottom left"><span><span><span class="text-line bkg-white mb-10"></span><span class="project-title">Jayden</span><span class="project-description">Personal Folio</span></span></span></span></a></div></div>',
         filter: function(image) {
             return image.tags.indexOf('gallery') >= 0;
         }
     });
-
-    userFeed.run();
-
-
+    //userFeed.run();
 
 });
 // id : https://codeofaninja.com/tools/find-instagram-user-id/
